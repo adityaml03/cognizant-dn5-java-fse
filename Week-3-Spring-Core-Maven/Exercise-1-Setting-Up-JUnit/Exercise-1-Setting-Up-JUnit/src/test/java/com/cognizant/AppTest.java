7a5e1a0d88413b04df4ea59f476f150e5cc8e3b2
adityaml03 <adityaml037@gmail.com>
2026-07-02 09:53:14 +0530

    Completed Week 3 Exercise 1 - JUnit setup

---
+++ Week-3-Spring-Core-Maven/Exercise-1-Setting-Up-JUnit/Exercise-1-Setting-Up-JUnit/src/test/java/com/cognizant/AppTest.java
@@ -0,0 +1,12 @@
+package com.cognizant;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
