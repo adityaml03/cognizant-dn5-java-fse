71b3612c90a71fa469902b4f57aef33feb73d43e
adityaml03 <adityaml037@gmail.com>
2026-07-09 12:03:47 +0530

    Completed Week 4 Exercise 1: Configured basic Spring application

---
+++ Week-4-Spring-Core/Exercise-1-Configuring-a-Basic-Spring-Application/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,12 @@
+package com.library.service;
+
+public class BookService {
+
+    public BookService() {
+        System.out.println("BookService bean created.");
+    }
+
+    public void service() {
+        System.out.println("BookService method called.");
+    }
+}
