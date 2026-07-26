9caa4fbe733b6635dde0c0f9b1e38873a3c08782
adityaml03 <adityaml037@gmail.com>
2026-07-26 20:46:32 +0530

    Completed Angular Hands-On 4

--- Week-7-Frontend-Git-Angular/student-course-portal/src/app/app.routes.ts
+++ Week-7-Frontend-Git-Angular/student-course-portal/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { CourseList } from './pages/course-list/course-list';
+import { EnrollmentFormComponent } from './pages/enrollment-form/enrollment-form';
+
+export const routes: Routes = [
+
+  { path: '', component: Home },
+
+  { path: 'courses', component: CourseList },
+
+  { path: 'enroll', component: EnrollmentFormComponent }
+
+];
--- Week-7-Frontend-Git-Angular/student-course-portal/src/app/app.ts
+++ Week-7-Frontend-Git-Angular/student-course-portal/src/app/app.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-import { Home } from './pages/home/home';
-import { CourseList } from './pages/course-list/course-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home, CourseList],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Header
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  title = 'student-course-portal';
-}
+export class App {}
